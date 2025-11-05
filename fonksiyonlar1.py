2d3ab9a87f5dc64450726e1b6974a09b6b7cb8cc
10B <keremuslu3801@gmail.com>
2025-11-05 15:13:48 +0300

    Initial commit

---
+++ fonksiyonlar1.py
@@ -0,0 +1,17 @@
+def ekranayaz(isim):
+    print(isim)
+
+
+def usal(taban,us):
+    sonuc=taban**us
+    return sonuc #return fonksiyonu sonucu hesaplar ama printin aksine ekrana yazdırmaz ve saklar
+
+
+isim="10B"
+ekranayaz(isim) #en üstteki fonksiyonun çalışma örneği
+
+print(usal(2,3))
+print(pow(2,3))
+
+
+
